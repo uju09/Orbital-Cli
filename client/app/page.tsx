7d4533d082ab223613d1d284f8fe659c4c7f94e9
uju09 <24bcs012@iiitdwd.ac.in>
2025-11-15 17:20:58 +0530

    Project Initialised

---
+++ client/app/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+
+
+
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen bg-gray-900">
+      <Button>Click me</Button>
+
+    </div>
+  );
+}
---
+++ server/src/index.js
@@ -0,0 +1,13 @@
+import Express from 'express';
+import dotenv from "dotenv";
+
+dotenv.config();
+const app = Express();
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}`);
+});
